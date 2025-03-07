8ac43e066a91e5f3cb05fece73ca2702e24c1808
jeroldleung <jeroldleung@gmail.com>
2025-03-07 15:44:28 +0800

    registry tools on llm provider

--- app/infra/llm_provider.py
+++ app/infra/llm_provider.py
@@ -1,4 +1,4 @@
-from openai import OpenAI
+from openai import OpenAI, pydantic_function_tool
 from openai.types.chat import (
     ChatCompletionMessage,
     ChatCompletionMessageParam,
@@ -10,15 +10,17 @@ class LLMProvider:
     def __init__(self, *, base_url: str, api_key: str, model: str):
         self.client = OpenAI(base_url=base_url, api_key=api_key)
         self.model = model
+        self.tools: list[ChatCompletionToolParam] | None = None
+
+    def registry_tools(self, *tools: ChatCompletionToolParam):
+        for t in tools:
+            self.tools.append(pydantic_function_tool(t))
 
     def chat_completion(
-        self,
-        *,
-        messages: list[ChatCompletionMessageParam],
-        tools: list[ChatCompletionToolParam] | None = None,
+        self, messages: list[ChatCompletionMessageParam]
     ) -> ChatCompletionMessage:
         completion = self.client.chat.completions.create(
-            model=self.model, messages=messages, tools=tools
+            model=self.model, messages=messages, tools=self.tools
         )
         res = completion.choices[0].message
         return res
