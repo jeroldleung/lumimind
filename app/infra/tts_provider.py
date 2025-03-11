60a43a1fa1606a00cc6bdf56ff4c732edb47f15d
jeroldleung <jeroldleung@gmail.com>
2025-03-11 20:33:43 +0800

    basic tts provider

--- app/infra/tts_provider.py
+++ app/infra/tts_provider.py
@@ -1,2 +1,38 @@
+import io
+
+import dashscope
+from dashscope.audio.tts_v2 import AudioFormat, ResultCallback, SpeechSynthesizer
+from loguru import logger
+
+
+class CallBack(ResultCallback):
+    _stream = None
+
+    def on_open(self):
+        self._stream = io.BytesIO()
+        logger.info("TTS websocket client connection is opened")
+
+    def on_complete(self):
+        logger.info("Speech synthesis task complete successfully")
+
+    def on_close(self):
+        self._stream.close()
+        logger.info("TTS websocket client connection is closed")
+
+    def on_data(self, data: bytes) -> None:
+        self._stream.write(data)
+
+
 class TTSProvider:
-    pass
+    def __init__(self, api_key: str, model: str):
+        dashscope.api_key = api_key
+        self.callback = CallBack()
+        self.synthesizer = SpeechSynthesizer(
+            model=model,
+            voice="longxiaochun",
+            format=AudioFormat.PCM_16000HZ_MONO_16BIT,
+            callback=self.callback,
+        )
+
+    def text2speech(self, text: str):
+        self.synthesizer.streaming_call(text)
