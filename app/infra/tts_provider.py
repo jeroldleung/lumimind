358b0d5993b4b3f637caac480541657e8eaa0825
jeroldleung <jeroldleung@gmail.com>
2025-03-13 00:00:25 +0800

    streaming tts

--- app/infra/tts_provider.py
+++ app/infra/tts_provider.py
@@ -1,21 +1,42 @@
 from typing import Generator
 
 import dashscope
-from dashscope.audio.tts_v2 import AudioFormat, SpeechSynthesizer
+from dashscope.audio.tts_v2 import AudioFormat, ResultCallback, SpeechSynthesizer
+from loguru import logger
+
+
+class CallBack(ResultCallback):
+    byte_queue = None
+
+    def on_open(self) -> None:
+        self.byte_queue = []
+        logger.info("TTS websocket connection is opened")
+
+    def on_data(self, data: bytes) -> None:
+        self.byte_queue.append(data)
+
+    def on_close(self) -> None:
+        logger.info("TTS websocket connection is closed")
 
 
 class TTSProvider:
     def __init__(self, api_key: str, model: str):
         dashscope.api_key = api_key
         self.model = model
+        self.callback = CallBack()
+        self.synthesizer = SpeechSynthesizer(
+            model=self.model,
+            voice="longxiaoxia_v2",
+            format=AudioFormat.WAV_16000HZ_MONO_16BIT,
+            callback=self.callback,
+        )
 
     def text2speech(
         self, stream_text: Generator[str, None, None]
     ) -> Generator[bytes, None, None]:
-        synthesizer = SpeechSynthesizer(
-            model=self.model,
-            voice="longxiaoxia_v2",
-            format=AudioFormat.WAV_16000HZ_MONO_16BIT,
-        )
         for text in stream_text:
-            yield synthesizer.call(text)
+            self.synthesizer.streaming_call(text)
+        self.synthesizer.streaming_complete()
+
+        for chunk in self.callback.byte_queue:
+            yield chunk
