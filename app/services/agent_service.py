f7b23ffae60047dd8ebf8a92134b0ab5e80921e0
jeroldleung <jeroldleung@gmail.com>
2025-03-21 16:27:41 +0800

    multiple function call

--- app/services/agent_service.py
+++ app/services/agent_service.py
@@ -1,11 +1,12 @@
 import json
+from typing import List
 
 from loguru import logger
+from openai.types.chat import ChatCompletionMessageToolCall
 from websockets.asyncio.server import ServerConnection
 
 from ..infra import IOTProvider, LLMProvider
 from ..schemas import iot_function_schemas as iotfs
-from ..utils.stream import accumulate_streaming
 
 
 class AgentService:
@@ -20,34 +21,46 @@ class AgentService:
         ts = [getattr(iotfs, name) for name in dir(iotfs) if name.startswith("iot")]
         self.llm_client.registry_tools(ts)
 
+    async def recur_tool_call(
+        self,
+        websocket: ServerConnection,
+        tool_calls: List[ChatCompletionMessageToolCall],
+    ) -> str:
+        self.messages.append({"role": "assistant", "tool_calls": tool_calls})
+        logger.debug(tool_calls)
+        for t in tool_calls:
+            tool_call, function = t, t.function
+            name, args = function.name, json.loads(function.arguments)
+            iot_method = getattr(self.iot_client, name)
+            result = await iot_method(websocket, args)
+            tool_msg = {
+                "role": "tool",
+                "tool_call_id": tool_call.id,
+                "content": str(result),
+            }
+            self.messages.append(tool_msg)
+
+        complete = self.llm_client.chat_completion(messages=self.messages)
+        if complete.choices[0].message.content:
+            return complete.choices[0].message.content
+        else:
+            return await self.recur_tool_call(
+                websocket, complete.choices[0].message.tool_calls
+            )
+
     async def chat_completion(
         self, websocket: ServerConnection, user_input: str
     ) -> str:
         self.messages.append({"role": "user", "content": user_input})
-        stream_msg = self.llm_client.chat_completion(self.messages)
-        content, tool_calls = accumulate_streaming(stream_msg)
-        self.messages.append({"role": "assistant", "content": content})
+        res_msg = self.llm_client.chat_completion(self.messages)
+        content = res_msg.choices[0].message.content
+        tool_calls = res_msg.choices[0].message.tool_calls
 
         # calling tools
         if tool_calls:
-            self.messages[-1]["tool_calls"] = tool_calls
-            logger.debug(tool_calls)
-
-            for t in tool_calls:
-                tool_call, function = t, t.function
-                name, args = function.name, json.loads(function.arguments)
-                iot_method = getattr(self.iot_client, name)
-                result = await iot_method(websocket, args)
-                tool_msg = {
-                    "role": "tool",
-                    "tool_call_id": tool_call.id,
-                    "content": str(result),
-                }
-                self.messages.append(tool_msg)
-
-            complete_stream = self.llm_client.chat_completion(messages=self.messages)
-            complete_content, _ = accumulate_streaming(complete_stream)
+            complete_content = await self.recur_tool_call(websocket, tool_calls)
             self.messages.append({"role": "assistant", "content": complete_content})
             return complete_content
 
+        self.messages.append({"role": "assistant", "content": content})
         return content
