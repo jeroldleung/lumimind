c212f74b8204c5886f8f4e8c67d73b4be35e4ef6
jeroldleung <jeroldleung@gmail.com>
2025-03-22 16:23:02 +0800

    add server host and port to config

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,6 +4,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
+    SERVER_HOST: str
+    SERVER_PORT: int
+
     LLM_BASE_URL: str
     LLM_API_KEY: str
     LLM_MODEL: str
